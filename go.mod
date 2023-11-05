7d02e0b8af9fd69a17445e79bba9668d3e148d96
Linkinlog <dahlton@dahlton.org>
2023-11-05 16:27:36 -0500

    feat: counterfeiter

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/Linkinlog/LeafList
 
 go 1.21.0
+
+require github.com/maxbrunsfeld/counterfeiter/v6 v6.7.0
+
+require (
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/tools v0.12.0 // indirect
+)
--- internal/graphql/client.go
+++ internal/graphql/client.go
@@ -4,6 +4,9 @@ import (
 	"context"
 )
 
+//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -generate
+
+//counterfeiter:generate . Client
 type Client interface {
 	Query(ctx context.Context, query string, variables map[string]interface{}, method string) ([]byte, error)
 }
