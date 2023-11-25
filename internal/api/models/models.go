5869f8be3722ad1565377fbcce8779ce3598f06f
Linkinlog <dahlton@dahlton.org>
2023-11-25 08:01:31 -0500

    fix: lowercased json responses

--- internal/api/models/models.go
+++ internal/api/models/models.go
@@ -1,42 +1,42 @@
 package models
 
 type Price struct {
-	Total           float64
+	Total           float64 `json:"total"`
 	DiscountedTotal float64 `json:"discountedTotal,omitempty"`
 }
 
 type Cannabinoid struct {
-	Name        string
-	Description string
-	Value       float64
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Value       float64 `json:"value"`
 }
 
 type Terpene struct {
-	Name        string
-	Description string
-	Value       float64
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Value       float64 `json:"value"`
 }
 
 type Category string
 
 type Variant struct {
-	Name  string `json:"Name,omitempty"`
-	Price *Price `json:"Price,omitempty"`
+	Name  string `json:"name,omitempty"`
+	Price *Price `json:"price,omitempty"`
 }
 
 type Product struct {
-	Id     string         `json:"id,omitempty"`
-	Name   string         `json:"name,omitempty"`
-	Ctg    Category       `json:"category,omitempty"`
+	Id     string         `json:"id"`
+	Name   string         `json:"name"`
+	Ctg    Category       `json:"category"`
 	Images []string       `json:"images"`
-	V      []*Variant     `json:"variant,omitempty"`
-	C      []*Cannabinoid `json:"cannabinoids,omitempty"`
-	T      []*Terpene     `json:"terpenes,omitempty"`
+	V      []*Variant     `json:"variants"`
+	C      []*Cannabinoid `json:"cannabinoids"`
+	T      []*Terpene     `json:"terpenes"`
 }
 
 type Offer struct {
-	Id          string
-	Description string
+	Id          string `json:"id"`
+	Description string `json:"description"`
 }
 
 type Dispensary struct {
